c3924c1382b3a6e12c552afe981599971a65b51f
Ashu Goyal <ashu@thena.ai>
2024-09-09 12:54:47 +0530

    last commit

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,44 +6,44 @@ const { TOKEN_PATH, CREDENTIALS_PATH, SCOPES } = require('../config/constants');
 
 
 // Not using the File way to store tokens anymore - Putting that env file
-/*
-async function loadSavedCredentialsIfExist() {
-    try {
-        const content = await fs.readFile(TOKEN_PATH);
-        const credentials= JSON.parse(content);
-        return google.auth.fromJSON(credentials);
-    } catch (err) {
-        return null;
-    }
-}
 
-async function saveCredentials(client) {
-    const content = await fs.readFile(CREDENTIALS_PATH);
-    const keys = JSON.parse(content);
-    const key = keys.installed || keys.web;
-    const payload = JSON.stringify({
-        type: 'authorized_user',
-        client_id: key.client_id,
-        client_secret: key.client_secret,
-        refresh_token: client.credentials.refresh_token,
-    });
-    await fs.writeFile(TOKEN_PATH, payload);
-}
+// async function loadSavedCredentialsIfExist() {
+//     try {
+//         const content = await fs.readFile(TOKEN_PATH);
+//         const credentials= JSON.parse(content);
+//         return google.auth.fromJSON(credentials);
+//     } catch (err) {
+//         return null;
+//     }
+// }
 
-async function authorize() {
-    let client = await loadSavedCredentialsIfExist();
-    if (!client) {
-        client = await authenticate({
-            scopes: SCOPES,
-            keyfilePath: CREDENTIALS_PATH,
-        });
-        if (client.credentials) {
-            await saveCredentials(client);
-        }
-    }
-    return client;
-}
-*/
+// async function saveCredentials(client) {
+//     const content = await fs.readFile(CREDENTIALS_PATH);
+//     const keys = JSON.parse(content);
+//     const key = keys.installed || keys.web;
+//     const payload = JSON.stringify({
+//         type: 'authorized_user',
+//         client_id: key.client_id,
+//         client_secret: key.client_secret,
+//         refresh_token: client.credentials.refresh_token,
+//     });
+//     await fs.writeFile(TOKEN_PATH, payload);
+// }
+
+// async function authorize1() {
+//     let client = await loadSavedCredentialsIfExist();
+//     if (!client) {
+//         client = await authenticate({
+//             scopes: SCOPES,
+//             keyfilePath: CREDENTIALS_PATH,
+//         });
+//         if (client.credentials) {
+//             await saveCredentials(client);
+//         }
+//     }
+//     return client;
+// }
+
 
 
 async function authorize1(){
